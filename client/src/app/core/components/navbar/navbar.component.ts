aef08c8b31334076a628c855729d9e3d451a71f4
Samuel Ruiz <samuel@ruizsamuel.es>
2025-08-06 18:52:19 +0200

    Add navbar

--- client/src/app/core/components/navbar/navbar.component.ts
+++ client/src/app/core/components/navbar/navbar.component.ts
@@ -1,8 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { LogoComponent } from "../../../shared/ui/logo/logo.component";
+import { RouterLink, RouterLinkActive } from "@angular/router";
+import { AuthService } from "../../../shared/service/auth.service";
 
 @Component({
   selector: "app-navbar",
   templateUrl: "./navbar.component.html",
+  imports: [LogoComponent, RouterLink, RouterLinkActive],
 })
 export class NavbarComponent {
+  authService = inject(AuthService);
 }
